a4f61b0c2164e7dce0d13e660b4e2644a9fc1d8e
LeeYeJin <104400282+L-yejin@users.noreply.github.com>
2023-06-13 01:50:59 +0900

    Add dataloaders

---
+++ BERT4Rec++/dataloaders/__init__.py
@@ -0,0 +1,13 @@
+from datasets import dataset_factory
+from .bert import BertDataloader
+
+DATALOADERS = {
+    BertDataloader.code(): BertDataloader
+}
+
+def dataloader_factory(args):
+    dataset = dataset_factory(args)
+    dataloader = DATALOADERS[args.dataloader_code]
+    dataloader = dataloader(args, dataset)
+    train, val, test, user_count, item_count = dataloader.get_pytorch_dataloaders()
+    return train, val, test
