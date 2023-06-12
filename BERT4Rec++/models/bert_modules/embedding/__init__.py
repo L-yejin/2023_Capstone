a4435766b6e90344cc54ccde4339e8240b14a316
LeeYeJin <104400282+L-yejin@users.noreply.github.com>
2023-06-13 02:06:18 +0900

    Add embedding folder

---
+++ BERT4Rec++/models/bert_modules/embedding/__init__.py
@@ -0,0 +1,11 @@
+from .bert import BERTEmbedding # 기존 Embedding
+from .bert import BERTEmbedding_Hyper # Hyperbolic Embedding
+
+EMBEDDING = {
+    BERTEmbedding.code(): BERTEmbedding,
+    BERTEmbedding_Hyper.code(): BERTEmbedding_Hyper
+}
+
+def model_embedding(args):
+    embedding = EMBEDDING[args.model_embedding]
+    return embedding
---
+++ BERT4Rec++/models/bert_modules/embedding/token.py
@@ -0,0 +1,7 @@
+import torch.nn as nn
+
+
+class TokenEmbedding(nn.Embedding):
+    def __init__(self, vocab_size, embed_size=512):
+        print(vocab_size,embed_size)
+        super().__init__(vocab_size, embed_size, padding_idx=0)
