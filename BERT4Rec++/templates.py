5cb0a7d9d54930bb8513d219116462951e66fc43
LeeYeJin <104400282+L-yejin@users.noreply.github.com>
2023-06-15 05:36:46 +0900

    Update templates.py
    test 할 수 있는 환경만들기 위해서 args 추가

--- BERT4Rec++/templates.py
+++ BERT4Rec++/templates.py
@@ -3,7 +3,8 @@ def set_template(args):
         return
 
     elif args.template.startswith('train_bert'):
-        args.mode = 'train'
+        args.mode = 'train' # 'train','test
+        args.test_model_path = None # test path
 
         args.dataset_code = 'ml-' + input('Input 1 for ml-1m, 20 for ml-20m: ') + 'm'
         args.min_rating = 0 if args.dataset_code == 'ml-1m' else 4
